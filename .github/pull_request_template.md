ce25f86493aa5fe34080d8791f24199886f7811b
Pablo Pessanha <pablopessanh@gmail.com>
2022-07-05 14:14:24 -0300

    feat: adiciona parte inicial da landing page

---
+++ .github/pull_request_template.md
@@ -0,0 +1,10 @@
+### Acompanhamento pelo Jira
+[BC-](https://breaking-the-code.atlassian.net/browse/BC-)
+
+### O que foi feito?
+ - Implementado tanana
+ - Corrigido pipipi popopo
+
+### Checklist
+- [ ] Meu código segue a conduta de boas práticas
+- [ ] As modificações foram validadas na minha máquina
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import LadingPage from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LadingPage />
   </React.StrictMode>
 );
