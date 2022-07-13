56290effc41df8da875a5bbcf894fdaa11d16432
Izabella <fsizabella25@gmail.com>
2022-07-13 18:58:07 -0300

    refactor: background fixes

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -3,25 +3,22 @@ import styles from './styles.module.scss';
 
 export default function HeroSection() {
   return (
-    <>
-      <section className={styles.backgroundImage} />
-      <section className={styles.hero}>
-        <header className={styles.heroHeader}>
-          <ul>
-            <li>Home</li>
-            <li>Product</li>
-            <li>Pricing</li>
-            <li>Contact</li>
-            <li>
-              <img src={figmaLandLogo} alt="logo da figmaland" />
-            </li>
-          </ul>
-        </header>
+    <section className={styles.hero}>
+      <header className={styles.heroHeader}>
+        <ul>
+          <li>Home</li>
+          <li>Product</li>
+          <li>Pricing</li>
+          <li>Contact</li>
+          <li>
+            <img src={figmaLandLogo} alt="logo da figmaland" />
+          </li>
+        </ul>
+      </header>
 
-        <h1>The best products start with Figma</h1>
-        <h2>Most calendars are designed for teams. Slate is designed for freelancers</h2>
-        <button type="button">Try For Free</button>
-      </section>
-    </>
+      <h1>The best products start with Figma</h1>
+      <h2>Most calendars are designed for teams. Slate is designed for freelancers</h2>
+      <button type="button">Try For Free</button>
+    </section>
   );
 }
